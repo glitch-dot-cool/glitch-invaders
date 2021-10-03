5faa9926a0b20a78b26319cc7e5a35f5e8513eb6
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2021-10-02 20:20:15 -0400

    🤞asset path fix 3: electric boogathree

--- src/utils/loadSprites.js
+++ src/utils/loadSprites.js
@@ -1,5 +1,5 @@
 export const loadSprites = (p5, fileNames, assetType) => {
   return fileNames.map((fileName) =>
-    p5.loadImage(`/assets/${assetType}/${fileName}.png`)
+    p5.loadImage(`assets/${assetType}/${fileName}.png`)
   );
 };

f0234824d5876d5c5459e0b62ff6693d46b03b4a
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2021-10-03 23:04:12 -0400

    add simple fetch class

---
+++ src/utils/fetch.js
@@ -0,0 +1,42 @@
+export class Fetch {
+  static baseUrl = "http://localhost:3000/invaders";
+
+  static async get(endpoint = "score") {
+    try {
+      const options = {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          Accept: "application/json",
+        },
+      };
+      const response = await fetch(`${this.baseUrl}/${endpoint}`, options);
+      if (response.ok) {
+        return response.json();
+      } else throw "error fetching score";
+    } catch (error) {
+      return error;
+    }
+  }
+
+  static async post(endpoint = "score", data) {
+    try {
+      const options = {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: data,
+      };
+      const response = await fetch(`${baseUrl}/${endpoint}`, options);
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw "error submitting score";
+      }
+    } catch (error) {
+      return error;
+    }
+  }
+}

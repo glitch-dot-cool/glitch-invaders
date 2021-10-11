744ab63a2e89859f725eb400008e84a0f2fc7a8b
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2021-10-11 17:14:01 -0400

    update prod api url

--- src/utils/setBaseUrl.js
+++ src/utils/setBaseUrl.js
@@ -1,7 +1,7 @@
 export const setBaseUrl = () => {
   const api = {
     dev: "http://localhost:3000/invaders",
-    prod: "http://172.104.209.136:3000/invaders",
+    prod: "https://droenbot.glitch.cool/invaders",
   };
   const prodUrls = [
     "https://glitch-dot-cool.github.io/glitch-invaders/",
